cc1b4418cd48ed5f6bfe4fe7e1255ff897a0d33a
Raffaele <raffaele.abramini@steellondon.com>
2016-03-07 15:15:52 +0000

    - run vagrant up after updating variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp'),
-    copy = require('gulp-copy');
+    fs = require('fs'),
+    copy = require('gulp-copy'),
+    replace = require('gulp-replace'),
+    vagrantVariables = JSON.parse(fs.readFileSync('./vagrant-variables.json'));
 
 
-gulp.task
+gulp.task('vagrant-setup', function(){
+    gulp.src('./Vagrantfile')
+        .pipe(replace('@__PathToVagrant__', vagrantVariables.PathToVagrant))
+        .pipe(replace('@__ProjectRoot__', vagrantVariables.ProjectRoot))
+        .pipe(replace('@__HostName__', vagrantVariables.HostName))
+        .pipe(replace('@__DBName__', vagrantVariables.DBName))
+        .pipe(replace('@__DBUser__', vagrantVariables.DBUser))
+        .pipe(replace('@__DBPsw__', vagrantVariables.DBPsw))
+        .pipe(gulp.dest('./'));
+
+    gulp.src('*.js', {read: false})
+        .pipe(shell('vagrant up'));
+});
