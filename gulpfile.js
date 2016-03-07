be98c27449ae8a1acbc85da7cb60ce9b32f56685
Raffaele <raffaele.abramini@steellondon.com>
2016-03-07 16:02:55 +0000

    - silverstripe installer - vagrantfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
     fs = require('fs'),
     copy = require('gulp-copy'),
     replace = require('gulp-replace'),
+    shell = require('gulp-shell'),
     vagrantVariables = JSON.parse(fs.readFileSync('./vagrant-variables.json'));
 
 
@@ -9,12 +10,14 @@ gulp.task('vagrant-setup', function(){
     gulp.src('./Vagrantfile')
         .pipe(replace('@__PathToVagrant__', vagrantVariables.PathToVagrant))
         .pipe(replace('@__ProjectRoot__', vagrantVariables.ProjectRoot))
-        .pipe(replace('@__HostName__', vagrantVariables.HostName))
+        .pipe(replace('@__ProjectName__', vagrantVariables.ProjectName))
         .pipe(replace('@__DBName__', vagrantVariables.DBName))
         .pipe(replace('@__DBUser__', vagrantVariables.DBUser))
         .pipe(replace('@__DBPsw__', vagrantVariables.DBPsw))
         .pipe(gulp.dest('./'));
 
     gulp.src('*.js', {read: false})
-        .pipe(shell('vagrant up'));
+        .pipe(shell([
+            'vagrant up'
+        ]));
 });
